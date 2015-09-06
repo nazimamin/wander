f994d86ed2dc2d67834a8271b24ffa550cb2e5ed
Gene Gurvich <genegurvich@gmail.com>
2015-09-05 21:29:28 -0400

    Add feature selection display logic, additional styles

--- wander/www/js/app.js
+++ wander/www/js/app.js
@@ -1,57 +1,43 @@
 // Wander App
 
-angular.module('wander', ['ionic'])
+wanderApp = angular.module('wander', ['ionic']);
 
-.controller('FeatureCtrl', function($scope) {
+wanderApp.controller('FeatureListCtrl', ['$scope', '$filter', function($scope, $filter) {
   $scope.features = [
-    {
-      "id": 1,
-      "name": "Waves",
-      "image": "img/thumb-surf.png"
-    },
-    {
-      "id": 2,
-      "name": "Deserts",
-      "image": "img/thumb-desert.png"
-    },
-    {
-      "id": 3,
-      "name": "Adventure",
-      "image": "img/thumb-touring.png"
-    },
-    {
-      "id": 4,
-      "name": "Relaxation",
-      "image": "img/thumb-canal.png"
-    },
-    {
-      "id": 5,
-      "name": "Waves",
-      "image": "img/thumb-surf.png"
-    },
-    {
-      "id": 6,
-      "name": "Deserts",
-      "image": "img/thumb-desert.png"
-    },
-    {
-      "id": 6,
-      "name": "Deserts",
-      "image": "img/thumb-desert.png"
-    },
-    {
-      "id": 6,
-      "name": "Deserts",
-      "image": "img/thumb-desert.png"
-    }
+    { "id": 1, "name": "Waves", "image": "img/thumb-surf.png" },
+    { "id": 2, "name": "Deserts", "image": "img/thumb-desert.png" },
+    { "id": 3, "name": "Adventure", "image": "img/thumb-touring.png" },
+    { "id": 4, "name": "Relaxation", "image": "img/thumb-canal.png" },
+    { "id": 5, "name": "Waves", "image": "img/thumb-surf.png" },
+    { "id": 6, "name": "Deserts", "image": "img/thumb-desert.png" },
+    { "id": 6, "name": "Deserts", "image": "img/thumb-desert.png" },
+    { "id": 6, "name": "Deserts", "image": "img/thumb-desert.png" },
+    { "id": 6, "name": "Deserts", "image": "img/thumb-desert.png" },
+    { "id": 6, "name": "Deserts", "image": "img/thumb-desert.png" }
   ];
 
-  $scope.toggleSelected = function() {
-    this.isSelected = !this.isSelected;
-  }
-})
+  $scope.minToSelect = 3;
 
-.run(function($ionicPlatform) {
+  $scope.minSelected = function() {
+    return ($scope.getSelectedFeatures().length >= 3) ? true : false;
+  }
+
+  $scope.getSelectedFeatures = function() {
+    return $filter('filter')($scope.features, { isSelected: "true" });
+  }
+
+  $scope.toggleSelected = function(feature) {
+    feature.isSelected = !feature.isSelected;
+  }
+
+  $scope.continue = function() {
+    // update user's selected features
+    // results = getMemes($scope.getSelectedFeatures());
+    // .then/.success/.catch navigate programmatically to next view
+  }
+}]);
+
+wanderApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -62,4 +48,4 @@ angular.module('wander', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
